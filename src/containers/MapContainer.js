70529bd57c65db6316a2a7dbccf3598adc68a8eb
rcrdlbl <bluh@outlook.com>
2019-03-15 18:43:38 -0400

    map now displays

---
+++ src/containers/MapContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import MapView from '../components/MapView'
+
+class MapContainer extends Component {
+  render() {
+    return (
+      <div>
+        <MapView></MapView>
+      </div>
+    )
+  }
+}
+
+export default MapContainer
