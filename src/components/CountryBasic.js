5358d1ea54bea57c8b17943591f781518d3c119c
rcrdlbl <bluh@outlook.com>
2019-04-02 12:16:10 -0400

    Added avara font

--- src/components/CountryBasic.js
+++ src/components/CountryBasic.js
@@ -6,6 +6,7 @@ class CountryBasic extends Component {
     return (
       <>
       <div className="countryBasic">
+        <Link to="/" className="searchLink">««search</Link>
         <h1>{this.props.countryInfo.name}</h1>
         <h1 className="countryNativeName">{this.props.countryInfo.nativeName}</h1>
         <img alt={"Flag of" + this.props.countryInfo.name} className="countryflag" src={this.props.countryInfo.flag}></img>
