b46b3d1e67ba376a0ec6803f7235ac8fdca0ac0b
rcrdlbl <bluh@outlook.com>
2019-04-05 21:10:16 -0400

    credits links

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MapView from '../components/MapView'
 import SearchView from '../components/SearchView'
+import { Link } from 'react-router-dom'
 
 
 class MapContainer extends Component {
@@ -13,6 +14,7 @@ class MapContainer extends Component {
       <div>
         <SearchView />
         <MapView className="frontMap"></MapView>
+        <div className='creditsLink'>WORLD FACTBOOK <Link to='/credits'>credits</Link> <a href='http://przemyslaw.org'>przemyslaw.org</a></div>
       </div>
     )
   }
